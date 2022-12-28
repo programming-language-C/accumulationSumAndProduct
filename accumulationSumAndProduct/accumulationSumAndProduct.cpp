e9ba73dbb8110bde74ef29fcac4201abebc803e1
Roman <maks3564195@gmail.com>
2022-12-28 16:24:10 +0300

    28.12.2022 initial commit

---
+++ accumulationSumAndProduct/accumulationSumAndProduct.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "Rus");
+	int i,
+	    S = 0,
+	    P = 1;
+	for (i = 1; i <= 3; i++)
+	{
+		S += i;
+		P *= i;
+	}
+	cout << "Сумма чисел от 1 до 3 равна " << S << endl;
+	cout << "Произведение чисел от 1 до 3 равна " << P << endl;
+	system("PAUSE");
+	return 0;
+}
